462b7c400be847efeb6c43b511e2b58fad7ab592
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-06-28 07:38:15 +0330

    add statistics section, cards and set counters

--- WebFrontend/script.js
+++ WebFrontend/script.js
@@ -1,6 +1,42 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+window.addEventListener('scroll', handleScroll);
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function updateCount(counter) {
+    const speed = 200;
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / speed;
+
+    if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(() => updateCount(counter), 5);
+    } else {
+        counter.innerText = target;
+    }
+}
+
+function handleScroll() {
+    const counters = document.querySelectorAll('.count');
+    counters.forEach(counter => {
+        if (isElementInViewport(counter) && !counter.classList.contains('counted')) {
+            counter.classList.add('counted');
+            updateCount(counter);
+        }
+    });
+}
+
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
